7a6bd871cbb97296d8db324fa116f1e27990ddc5
Ziyad Hussain <ziyad@eazio.com>
2023-08-18 06:22:58 -0700

    change local base URL

--- src/constant/environment.ts
+++ src/constant/environment.ts
@@ -1,3 +1,3 @@
 // export const API_BASE_URL = 'http://192.168.100.4:3000/';
-//export const API_BASE_URL = 'http://192.168.100.4:3000/';
-export const API_BASE_URL = 'http://3.29.128.111:3000';
+export const API_BASE_URL = 'http://192.168.100.4:3000/';
+//export const API_BASE_URL = 'http://3.29.128.111:3000';
